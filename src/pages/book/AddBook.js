c51ad2eca8a224ed5acf37756847d76e48d55ecc
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-19 16:06:31 +0300

    Add BookFrom.js and move Form component of AddBook

--- src/pages/book/AddBook.js
+++ src/pages/book/AddBook.js
@@ -14,7 +14,6 @@ import BookForm from "./BookForm";
 
 export default function AddBook() {
 
-  const [componentSize, setComponentSize] = useState("default");
   const [bookData, setBookData] = useState(
     {
       name: "",
@@ -25,10 +24,6 @@ export default function AddBook() {
       publicationDate: ""
     }
   );
-
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
-  };
 
   function onBookDataChange(event) {
     const { name, value } = event.target;
@@ -49,98 +44,7 @@ export default function AddBook() {
 
   return (
     <div>
-      {/* <Form
-        labelCol={{
-          span: 4
-        }}
-        wrapperCol={{
-          span: 14
-        }}
-        layout="horizontal"
-        initialValues={{
-          size: componentSize
-        }}
-        onValuesChange={onFormLayoutChange}
-        size={componentSize}
-        onFinish={onBookFormFinish}
-        onFinishFailed={onBookFormFinish}
-      >
-        <Form.Item label="Form Size" name="size">
-          <Radio.Group>
-            <Radio.Button value="small">Small</Radio.Button>
-            <Radio.Button value="default">Default</Radio.Button>
-            <Radio.Button value="large">Large</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Name" onChange={onBookDataChange} rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          }]}>
-          <Input
-            id="name"
-            name="name"
-            value={bookData.name}
-            placeholder="Book Name"
-          />
-        </Form.Item>
-
-        <Form.Item label="Author" onChange={onBookDataChange}>
-          <Input
-            id="author"
-            name="author"
-            value={bookData.author}
-            placeholder="Author"
-          />
-        </Form.Item>
-
-        <Form.Item label="Page Count" onChange={onBookDataChange}>
-          <InputNumber
-            min={0}
-            id="pageCount"
-            name="pageCount"
-            value={bookData.pageCount}
-            controls={false}
-            placeholder="Page Count"
-          />
-        </Form.Item>
-
-        <Form.Item label="Type" onChange={onBookDataChange}>
-          <Input
-            id="type"
-            name="type"
-            value={bookData.type}
-            placeholder="Type"
-          />
-        </Form.Item>
-
-        <Form.Item label="Publisher" onChange={onBookDataChange}>
-          <Input
-            id="publisher"
-            name="publisher"
-            value={bookData.publisher}
-            placeholder="Publisher"
-          />
-        </Form.Item>
-
-        <Form.Item label="Publication Date" onChange={onBookDataChange}>
-          <Input
-            type="date"
-            id="publicationDate"
-            name="publicationDate"
-            value={bookData.publicationDate}
-            defaultValue="1970-01-01"
-          />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form> */}
-
-      <BookForm bookData={bookData} handleBookChange={onBookDataChange} />
+      <BookForm bookData={bookData} handleBookChange={onBookDataChange} onFormFinish={onBookFormFinish} />
 
       <Descriptions
         title="Book Info"
