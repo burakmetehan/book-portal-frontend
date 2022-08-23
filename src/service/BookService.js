a26f8e647f80e0a1e6264b3e267ee1a25324b261
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-23 15:42:58 +0300

    Update BookService.js

--- src/service/BookService.js
+++ src/service/BookService.js
@@ -1,86 +1,3 @@
-export function _searchAll(pagination) {
-
-  var axios = require('axios');
-  var data = JSON.stringify({
-  });
-
-  var config = {
-    method: 'get',
-    url: `http://localhost:8080/books?pageNumber=${pagination.pageNumber}&pageSize=${pagination.pageSize}`,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('Authorization')
-    },
-    data: data
-  };
-
-  return axios(config)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log("Error in BookService!");
-      return;
-    });
-}
-
-export function _searchById({ bookId }) {
-
-  var axios = require('axios');
-  var data = JSON.stringify({
-  });
-
-  var config = {
-    method: 'get',
-    url: `http://localhost:8080/books/${bookId}`,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('Authorization')
-    },
-    data: data
-  };
-
-  return axios(config)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log("Error in BookService!");
-      return;
-    });
-}
-
-/**
- * Searching books that contains the given bookName.
- * @param {String} bookName Name of the book(s) to be searched
- * @returns Paged book(s)
- */
-export function _searchByName({ bookName }) {
-
-  var axios = require('axios');
-  var data = JSON.stringify({
-  });
-
-  var config = {
-    method: 'get',
-    url: `http://localhost:8080/books/name/${bookName}`,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('Authorization')
-    },
-    data: data
-  };
-
-  return axios(config)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log("Error in BookService!");
-      return;
-    });
-}
-
 export function _addBook({
   name,
   author,
@@ -112,18 +29,22 @@ export function _addBook({
 
   return axios(config)
     .then(function (response) {
-      return response.data;
+      return {
+        successful: true,
+        ...response.data
+      };
     })
-    .catch(function (error) {
-      console.log("Error in BookService!");
-      return;
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
     });
 }
 
 export function _deleteBook({ bookId }) {
 
   var axios = require('axios');
-
   var config = {
     method: 'delete',
     url: `http://localhost:8080/books/${bookId}`,
@@ -135,12 +56,133 @@ export function _deleteBook({ bookId }) {
 
   return axios(config)
     .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      return response.data;
+      return {
+        successful: true,
+        ...response.data
+      };
     })
-    .catch(function (error) {
-      console.log("Error in BookService!");
-      return error;
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
     });
 }
 
+export function _searchAllBook(pagination) {
+
+  var axios = require('axios');
+  var data = JSON.stringify({});
+
+  const pageNumber = pagination.pageNumber || 0;
+  const pageSize = pagination.pageSize || 10;
+
+  var config = {
+    method: 'get',
+    url: `http://localhost:8080/books?pageNumber=${pageNumber}&pageSize=${pageSize}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('Authorization')
+    },
+    data: data
+  };
+
+  return axios(config)
+    .then(function (response) {
+      return {
+        successful: true,
+        ...response.data
+      };
+    })
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
+    });
+}
+
+export function _searchBookById({ bookId }) {
+
+  var axios = require('axios');
+  var data = JSON.stringify({});
+
+  var config = {
+    method: 'get',
+    url: `http://localhost:8080/books/${bookId}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('Authorization')
+    },
+    data: data
+  };
+
+  return axios(config)
+    .then(function (response) {
+      return {
+        successful: true,
+        ...response.data
+      };
+    })
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
+    });
+}
+
+/**
+ * Searching books that contains the given bookName.
+ * @param {String} bookName Name of the book(s) to be searched
+ * @returns Paged book(s)
+ */
+export function _searchBookByName({ bookName }) {
+
+  var axios = require('axios');
+  var data = JSON.stringify({});
+
+  var config = {
+    method: 'get',
+    url: `http://localhost:8080/books/name/${bookName}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('Authorization')
+    },
+    data: data
+  };
+
+  return axios(config)
+    .then(function (response) {
+      return {
+        successful: true,
+        ...response.data
+      };
+    })
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
+    });
+}
+
+export function _updateBook() {
+  // @TODO
+
+  return false;
+
+  return axios(config)
+    .then(function (response) {
+      return {
+        successful: true,
+        ...response.data
+      };
+    })
+    .catch(function () {
+      console.error("Error in adding book!")
+      return {
+        successful: false
+      };
+    });
+}
