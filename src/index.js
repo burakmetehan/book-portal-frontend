7d819ddd895fb326496f3f065c0bf7116df818da
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-24 15:58:43 +0300

    Minor updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import AddBook from "./pages/book/AddBook";
-import UpdateBook from "./pages/book/UpdateBook";
-import DeleteBook from "./pages/book/DeleteBook"
 
-import AddUser from "./pages/user/AddUser";
-import DeleteUser from "./pages/user/DeleteUser";
-
-import UserShow from './pages/user/UserShow';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
   <React.StrictMode>
-    <DeleteUser />
+    <App />
   </React.StrictMode>
 );
