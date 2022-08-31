0c8163d7319ab93e5d9c23a8110cfb023bd96789
Burak Metehan Tunçel <burakmetehan.123@hotmail.com>
2022-08-31 19:00:03 +0300

    Update AddBook

--- src/pages/book/add/AddBook.js
+++ src/pages/book/add/AddBook.js
@@ -2,18 +2,27 @@ import { notification } from "antd";
 import "antd/dist/antd.css";
 import React, { useState } from "react";
 
-import BookDescription from "./BookDescription";
-import BookForm from "./BookForm";
+import BookDescription from "../util/BookDescription";
+import BookForm from "../util/BookForm";
 
-import { _addBook } from "../../service/BookService";
+import { _addBook } from "../../../service/BookService";
+import { BookListParser } from "../util/BookContentParser";
 
 export default function AddBook() {
-
   const [isSuccessful, setIsSuccessful] = useState(false);
-  const [bookData, setBookData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     author: "",
-    pageCount: "",
+    pageCount: 0,
+    type: "",
+    publisher: "",
+    publicationDate: ""
+  })
+  const [bookData, setBookData] = useState({
+    id: 0,
+    name: "",
+    author: "",
+    pageCount: 0,
     type: "",
     publisher: "",
     publicationDate: ""
@@ -23,20 +32,44 @@ export default function AddBook() {
   function handleBookDataChange(event) {
     const { name, value } = event.target;
 
-    setBookData((prevBookData) => ({
-      ...prevBookData,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       [name]: value
     }));
   }
 
   async function handleBookFormFinish() {
-    const response = await _addBook(bookData);
+    const response = await _addBook(formData);
 
-    if (response.successful) {
-      setIsSuccessful(true);
-    } else {
+    if (!response.successful) {
+      const config = {
+        message: 'Please, fill the all necessary fields!',
+        description: '',
+        duration: 4.5,
+        key: 'add-book-error',
+        placement: 'top'
+      }
+
+      notification.error(config);
+
       setIsSuccessful(false);
+      return;
     }
+
+    const config = {
+      message: 'Book is added!',
+      description: 'Book is successfully added!',
+      duration: 4.5,
+      key: 'add-book-success',
+      placement: 'top'
+    }
+
+    notification.success(config);
+
+    const newContent = BookListParser(response.data);
+
+    setIsSuccessful(true);
+    setBookData(newContent[0]);
   }
 
   function handleBookFormFail() {
